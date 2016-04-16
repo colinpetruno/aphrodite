8fa40e7fe264df15cd907f3a0368e62733ef1df8
Colin Petruno <colinpetruno@gmail.com>
2016-04-16 11:03:02 -0400

    Prod Config Updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,14 +36,14 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://aphroditecss.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://aphroditecss.com', /https:\/\/aphroditecss.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
