03e6afb93c0072babcda3d1835e8de2801c207fd
Colin Petruno <colinpetruno@gmail.com>
2016-05-14 23:41:51 -0400

    Switch to table view for Stylesheet Index

--- app/controllers/application/stylesheets_controller.rb
+++ app/controllers/application/stylesheets_controller.rb
@@ -2,7 +2,7 @@ class Application::StylesheetsController < AuthenticatedController
   skip_before_action :authenticate_user!, only: [:show]
 
   def index
-    @stylesheets = current_account.stylesheets
+    @stylesheets = current_account.stylesheets.order(:name)
   end
 
   def new
