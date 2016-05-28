01aae76ce338e128cd9adf0bb0d2ce619471bff2
Colin Petruno <colinpetruno@gmail.com>
2016-05-28 10:17:21 -0400

    Set Up Preview Link

--- app/models/account.rb
+++ app/models/account.rb
@@ -9,4 +9,15 @@ class Account < ApplicationRecord
   has_one :slack_authentication
 
   accepts_nested_attributes_for :users
+
+
+  before_validation :add_url_protocol
+
+  protected
+
+  def add_url_protocol
+    unless self.site_url[/\Ahttp:\/\//] || self.site_url[/\Ahttps:\/\//]
+      self.site_url = "http://#{self.site_url}"
+    end
+  end
 end
