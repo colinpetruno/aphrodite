eeb0806a200d9ddcb6d52ae38ce37234ccb72f5a
Colin Petruno <colinpetruno@gmail.com>
2016-07-21 23:04:07 -0400

    Somethings broke in prod with this
    when getting the key from secrets it truncates digits, but the
    env is set correctly and works /shrug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :slack,
-    Rails.application.secrets.slack_client_id,
+    (ENV["SLACK_CLIENT_ID"] || Rails.application.secrets.slack_client_id),
     Rails.application.secrets.slack_client_secret,
     scope: "identify,read,post"
 end
