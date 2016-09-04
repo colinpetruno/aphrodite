a8729dabefe1cdd647fcd9e309bc10e65f8119e7
Colin Petruno <colinpetruno@gmail.com>
2016-09-04 11:59:07 +0200

    Unbreak other assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
 
-  config.action_controller.asset_host = "aphroditecss.com"
   config.action_mailer.asset_host = "https://www.aphroditecss.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
