5520cdbfbb81956aab7b1ad7b0151340cbbbef8c
Colin Petruno <colinpetruno@gmail.com>
2016-09-04 11:51:08 +0200

    Fiddle with Asset Hosts
    Images in mailers still not working properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: "https://www.aphroditecss.com" }
+  config.action_controller.asset_host = "aphroditecss.com"
+  config.action_mailer.asset_host = "https://www.aphroditecss.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
